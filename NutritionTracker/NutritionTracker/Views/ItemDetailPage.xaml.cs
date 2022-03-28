090b75d61a797917753f99f63d183f7884a48c7b
Eru Findlay <1611662@studentmail.ucol.ac.nz>
2022-03-28 14:45:30 +1300

    Add project files.

---
+++ NutritionTracker/NutritionTracker/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using NutritionTracker.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace NutritionTracker.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ NutritionTracker/NutritionTracker/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using NutritionTracker.Models;
+using NutritionTracker.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NutritionTracker.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
