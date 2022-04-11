3176985ef56cdf2b72a5a13408caed85d6845b62
Eru Findlay <1611662@studentmail.ucol.ac.nz>
2022-04-11 15:09:32 +1200

    Added some viewmodels based off the databasemanager

--- NutritionTracker/NutritionTracker/ViewModels/SearchFoodViewModel.cs
+++ NutritionTracker/NutritionTracker/ViewModels/SearchFoodViewModel.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NutritionTracker.Data;
+using NutritionTracker.Models;
 
 namespace NutritionTracker.ViewModels
 {
-    class SearchFoodViewModel
+    public class SearchFoodViewModel
     {
+        public SearchFoodViewModel() { }
+
+        private databaseManager dbm = new databaseManager(databaseSettings.defaultPath);
+
+        private string _searchString;
+        private List<foodItem> _searchResults;
+
+        public string searchString
+        {
+            get { return _searchString; }
+            set { _searchString = value; }
+        }
+
+        public List<foodItem> searchResults
+        {
+            get { return _searchResults; }
+            set { _searchResults = value; }
+        }
+
+
+        public List<foodItem> getSearchResults()                    //This could either be triggered by a keystroke in the search bar or by clicking a search button
+        {
+            return dbm.getFoodItemByNameAsync(searchString);
+        }
     }
 }
