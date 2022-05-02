db42d68f983b0f38a463166f13140c445cf4e4e7
1826083@ucolcloud.ac.nz <1826083@ucolcloud.ac.nz>
2022-05-02 14:38:15 +1200

    trying to make profile page

--- NutritionTracker/NutritionTracker/ViewModelsTests/LoginViewModelTest.cs
+++ NutritionTracker/NutritionTracker/ViewModelsTests/LoginViewModelTest.cs
@@ -14,11 +14,12 @@ namespace NutritionTracker.ViewModels
         {
             LoginCommand = new Command(OnLoginClicked);
         }
-
+        
         private async void OnLoginClicked(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
             await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            //have this link to a profile page? or have th option once logged in?
         }
     }
 }
