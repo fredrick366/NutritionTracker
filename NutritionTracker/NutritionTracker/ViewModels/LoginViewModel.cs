96629eb98b741c212a04a1247cb22c69c121b977
Zion Tawaroa <tawaroaz@gmail.com>
2022-05-15 21:51:37 +1200

    LoginPage and LoginViewModel test

---
+++ NutritionTracker/NutritionTracker/ViewModels/LoginViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace NutritionTracker.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public Action DisplayInvalidLoginPrompt;
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Username"));
+            }
+        }
+        private string password;
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+        public ICommand LoginCommand { protected set; get; }
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnSubmit);
+        }
+        public void OnSubmit()
+        {
+            if (username != "tkzhyon" || password != "secret")
+            {
+                DisplayInvalidLoginPrompt();
+            }
+        }
+    }
+}
