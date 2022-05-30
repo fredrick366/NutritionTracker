baf90712cedd093d0789fcb453f3a2bc118ddacd
Eru Findlay <1611662@studentmail.ucol.ac.nz>
2022-05-30 14:42:19 +1200

    test

---
+++ NutritionTracker/NutritionTracker/Views/FoodItemEntrySettingsPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NutritionTracker.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FoodItemEntrySettingsPage : ContentPage
+    {
+        FoodItemEntrySettingsPage _viewModel;
+
+        public FoodItemEntrySettingsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new FoodItemEntrySettingsPage();
+        }
+    }
+}
